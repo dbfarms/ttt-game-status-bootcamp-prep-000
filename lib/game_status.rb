182ad1545f602d708a94381dd3a045ebeddf8fad
Matthew Hunger <matt.hunger@gmail.com>
2017-10-03 09:55:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+
+  WIN_COMBINATIONS.each | win_index |
+    win_index.each do 
+      i = 0
+      while i <= 3
+        if board[i] == win_index[i]
+      end 
+    end
+  end
+
+end
